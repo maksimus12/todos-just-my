41e2639b6519357f342addb5d862e6cb7719199a
Max <maks.diacenko@mail.ru>
2020-09-26 15:14:48 +0500

    Updates

---
+++ precache-manifest.c03916f90cdd27ce275e893784c51319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6294771a3e3f1dc8b8ae99c7c2293b",
+    "url": "/http//maksimus12.github.io/todo-just-my/index.html"
+  },
+  {
+    "revision": "a0fafc2fdc05dd007095",
+    "url": "/http//maksimus12.github.io/todo-just-my/static/css/main.6ce1b27e.chunk.css"
+  },
+  {
+    "revision": "735e3a42a953643d6403",
+    "url": "/http//maksimus12.github.io/todo-just-my/static/js/2.12e6d84c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//maksimus12.github.io/todo-just-my/static/js/2.12e6d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fafc2fdc05dd007095",
+    "url": "/http//maksimus12.github.io/todo-just-my/static/js/main.014dc1e6.chunk.js"
+  },
+  {
+    "revision": "848b6009c5fda2fe17a6",
+    "url": "/http//maksimus12.github.io/todo-just-my/static/js/runtime-main.64f68a1b.js"
+  }
+]);
